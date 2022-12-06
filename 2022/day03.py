51855999515748eabd047b961d5f953fa09ff750
coale <10050779+coale@users.noreply.github.com>
2022-12-06 11:23:28 +0100

    added part2

--- 2022/day03.py
+++ 2022/day03.py
@@ -1,6 +1,6 @@
 import string
 
-with open("input.txt") as input:
+with open("inputs/input_day03.txt") as input:
   f = input.readlines()
 
 #Part 1
@@ -25,3 +25,32 @@ total_priority = 0
 for line in f:
   total_priority += values_dict[divide_comp(line)]
 print(total_priority)
+
+#Part 2
+list_of_groups = [[] for item in range(1, 101)]
+i = 0
+for line in f:
+  if len(list_of_groups[i]) == 3:
+    i += 1
+    list_of_groups[i].append(line)
+  else:
+    list_of_groups[i].append(line)
+
+def find_groups(list):
+  elf1 = set()
+  elf2 = set()
+  elf3 = set()
+  for letter in list[0][:-1]:
+    elf1.add(letter)
+  for letter in list[1][:-1]:
+    elf2.add(letter)
+  for letter in list[2][:-1]:
+    elf3.add(letter)
+  badge = elf1.intersection(elf2, elf3)
+  for item in badge:
+    return item
+
+total_badge_priority = 0
+for list in list_of_groups:
+  total_badge_priority += values_dict[find_groups(list)]
+print(total_badge_priority)
